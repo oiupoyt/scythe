5cb25e3ba9548c77c01b669939d9c3944444655f
oiupoyt <oiupoyt.cat@gmail.com>
2026-09-02 19:30:07 +0530

    chore: Clean up MP4 files and test scripts from repository

--- test_stop.py
+++
@@ -1,7 +0,0 @@
-import socket, struct, os
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-sock.connect(os.environ.get("XDG_RUNTIME_DIR", "/tmp") + "/vrec.sock")
-payload = b'"StopRecording"'
-sock.sendall(struct.pack('<I', len(payload)))
-sock.sendall(payload)
-print("Sent StopRecording")
